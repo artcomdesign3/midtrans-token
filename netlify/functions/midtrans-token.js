e57bbfde8698c32374ead9456ec561560ccb9cb9
artcomdesign3 <artcomdesign3@gmail.com>
2025-08-24 01:22:42 +0300

    Update midtrans-token.js

--- netlify/functions/midtrans-token.js
+++ netlify/functions/midtrans-token.js
@@ -62,9 +62,7 @@ exports.handler = async function(event, context) {
 
         // Midtrans API URL (Production)
         const api_url = 'https://app.midtrans.com/snap/v1/transactions';
-        
-        // Get server key from environment variable
-        const server_key = process.env.MIDTRANS_SERVER_KEY || 'Mid-server-kO-tU3T7Q9MYO_25tJTggZeu';
+        const server_key = 'Mid-server-kO-tU3T7Q9MYO_25tJTggZeu';
 
         // Make API call to Midtrans
         const response = await fetch(api_url, {
